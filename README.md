4099aecb849d102121daecd59ef281fbd41bc374
nikhil sadakar <niksad8@gmail.com>
2022-02-21 13:16:28 +0530

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # wowautomation-csharp
+
+There are 2 programs here one is called wowinstance and other is wowlauncher
+
+# wowinstance
+
+this is the main program that will open
+- get data from arguments passed to the program, eg : username, password, realm, auctioneer name
+- setup realm.wtf file to correct realm name
+- setup correct realm so that we dont get a realm select screen
+- open wow
+- login user
+- enter the character select screen
+- enter the game type `/tar <auctioneer name>`
+- interact with the selected npc using the keybind passed
+- make sure the auction screen is open 
+- start scan using `/auc scan`
+- once scan is complete the wow.exe will close
+- once the application is closed, 
+
+the automation program will do a pixel check on all the pages to see that we are in the correct screen. Eg: login screen we test for a login button to check we are on the login screen
+
+If the program finds out that we are in the wrong screen then we just terminate the program and we return a non 0 exit code to report an error.
+
+This program needs a config file below are a sample of what is needed.
+
+```[config]
+base_url=https://www.web-auctioneer.com/
+query_url=private-api/getrealmdata
+wow_path_wotlk=H:\World Of Warcraft-335-clean\World.of.Warcraft.3.3.5a\
+wow_path_tbc=E:\downloads\TBC-2.4.3.8606-Repack\TBC-2.4.3.8606-Repack\
+```
+# wowlauncher
+
+this program will communicate with the server and finds out if any scans are pending
+
+- poll server to check if any scans are pending
+- if there are scans pending, get all the data eg: username, password, realm_id, faction, auctioneer name 
+- pass all this data to wowinstance.exe 
+- check if that wowinstance is still open, if it has closed then check the return code, non zero return code is error, if there is an error just do nothing and move on
+- if the return code is 0 (success) read the (`WTF\\Account\\<account_name>\\SavedVariables\\Auc-ScanData.lua`) aucscan data and upload the file to the server. 
+
+# How to set up wow (wotlk, tbc right now)
+- the wow.exe should be always called `wow.exe`
+- set the resolution to 800x600( i have tested on this only). we do pixel tests i have not tested it on other resolutions
+- place your bot char near an auctioneer, and note the name, you should be able to highlight the auctioneer using the command `/tar <auctioneer_name>`
+- bind a key for the action interact with character i set it to `]`. this key will be set in your realm data field.
+- make sure you can interact with the auctioneer with this new key
+- open up the auction house and make sure the window is on top left corner of the screen, as far as you can push it. If not the luancher pixel test will fail.
+- in the auctioneer setting, go to the advanced setttings and look for `When scan is complete` section and select it to shutdown client.
+- this is important as we will know when the scan is complete. when the application closes by itself
+
---
+++ wowlauncher/wowlauncher/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Collections;
+
+class WoWInstance
+{
+    public int realm_id;
+    public string realm_name;
+    public string account_name;
+    public string password;
+    public string auctioneer_name;
+    public string faction;
+    public int last_modified_date;
+    public long  size;
+    public string connection_url;
+    public Process wowprocess;
+}
+namespace wowlauncher
+{
+    class IniFile   // revision 11
+    {
+        string Path;
+        string EXE = Assembly.GetExecutingAssembly().GetName().Name;
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string FilePath);
+
+        public IniFile(string IniPath = null)
+        {
+            Path = new FileInfo(IniPath ?? EXE + ".ini").FullName.ToString();
+        }
+
+        public string Read(string Key, string Section = null)
+        {
+            var RetVal = new StringBuilder(255);
+            GetPrivateProfileString(Section ?? EXE, Key, "", RetVal, 255, Path);
+            return RetVal.ToString();
+        }
+
+        public void Write(string Key, string Value, string Section = null)
+        {
+            WritePrivateProfileString(Section ?? EXE, Key, Value, Path);
+        }
+
+        public void DeleteKey(string Key, string Section = null)
+        {
+            Write(Key, null, Section ?? EXE);
+        }
+
+        public void DeleteSection(string Section = null)
+        {
+            Write(null, null, Section ?? EXE);
+        }
+
+        public bool KeyExists(string Key, string Section = null)
+        {
+            return Read(Key, Section).Length > 0;
+        }
+    }
+    class Program
+    {
+        
+
+        public string Get(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            string id = "";
+            string faction = "";
+
+            if (args.Length > 0)
+                id = args[0];
+            if (args.Length > 1)
+                faction = args[1];
+
+            ArrayList process_data = new ArrayList();
+            dynamic realms;
+            Console.WriteLine("Reading config file...");
+            var ini = new IniFile(Directory.GetCurrentDirectory()+"\\config.ini");
+            var base_url = ini.Read("base_url", "config");
+            var query_url = ini.Read("query_url", "config");
+            var wow_path_wotlk = ini.Read("wow_path_wotlk", "config");
+            var wow_path_tbc = ini.Read("wow_path_tbc", "config");
+            while (true) // loop always
+            {
+                var start_time_stamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+                var fetch_url = base_url + "scheduler/index";
+                var json_str2 = new WebClient().DownloadString(fetch_url);
+                var realm_id = "";
+                var faction_id = "";
+                dynamic schedule_json = JsonConvert.DeserializeObject(json_str2);
+                for (var c = 0; c < schedule_json.Count; c++)
+                {
+                    realm_id = schedule_json[c].realm_id;
+                    faction_id = schedule_json[c].faction_id;
+                    var final_url = base_url + query_url + "?&id=" + realm_id + "&f=" + faction_id;
+                    try
+                    {
+                        var json_str = new WebClient().DownloadString(final_url);
+                        dynamic json = JsonConvert.DeserializeObject(json_str);
+                        realms = json;
+                        for (var i = 0; i < json.Count; i++)
+                        {
+                            var expac = json[i].expansion;
+                            var wow_path = wow_path_wotlk;
+                            if (expac == "3.3.5")
+                                wow_path = wow_path_wotlk;
+                            else if (expac == "2.4.3")
+                                wow_path = wow_path_tbc;
+
+                            WoWInstance wowdata = new WoWInstance();
+                            wowdata.wowprocess = new Process();
+                            wowdata.wowprocess.StartInfo.FileName = wow_path + "wowinstance.exe";
+                            wowdata.wowprocess.StartInfo.WorkingDirectory = wow_path;
+                            wowdata.realm_name = json[i].name;
+                            wowdata.connection_url = json[i].connection_url;
+                            wowdata.realm_id = json[i].id;
+
+                            wowdata.account_name = json[i].data.username;
+                            wowdata.auctioneer_name = json[i].data.auctioneer;
+                            wowdata.faction = json[i].faction;
+                            wowdata.password = json[i].data.password;
+                            string aucdata = wow_path + "WTF\\Account\\" + wowdata.account_name.ToUpper() + "\\SavedVariables\\Auc-ScanData.lua";
+                            if (File.Exists(aucdata))
+                            {
+                                wowdata.size = new System.IO.FileInfo(aucdata).Length;
+                            }
+                            else
+                            {
+                                wowdata.last_modified_date = 0;
+                                wowdata.size = 0;
+                            }
+                            wowdata.wowprocess.StartInfo.Arguments = wowdata.realm_id + " \"" + wowdata.realm_name + "\" \"" + wowdata.connection_url + "\" " + wowdata.account_name + " \"" + wowdata.password + "\" \"" + wowdata.auctioneer_name + "\" \"" + base_url + "\" " + wowdata.faction;
+                            Console.WriteLine("Start ..." + wowdata.wowprocess.StartInfo.Arguments);
+                            if (wowdata.wowprocess.Start())
+                            {
+                                Console.WriteLine("Process for realm id " + json[i].id);
+                                process_data.Add(wowdata);
+                            }
+                            System.Threading.Thread.Sleep(60000);
+                        }
+                    }
+                    catch (WebException e) 
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                while (process_data.Count > 0)
+                {
+                    for (var i = 0; i < process_data.Count; i++)
+                    {
+                        WoWInstance wow = (WoWInstance)process_data[i];
+                        if (wow.wowprocess.HasExited)
+                        {
+                            if (wow.wowprocess.ExitCode == 0) // everything OK
+                            {
+                                Console.WriteLine("Realm " + wow.realm_name + " " + wow.faction + " eneded successfully");
+                            }
+                            else if (wow.wowprocess.ExitCode == 1) // start failure
+                            {
+                                Console.WriteLine("Realm " + wow.realm_name + " " + wow.faction + " start failed");
+                            }
+                            else if (wow.wowprocess.ExitCode == 2) // login or script problem
+                            {
+                                Console.WriteLine("Realm " + wow.realm_name + " " + wow.faction + " scripting problem");
+                            }
+                            process_data.RemoveAt(i);
+                        }
+                    }
+                    System.Threading.Thread.Sleep(1000);
+                }
+                var current_timestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+                var timeleft = (current_timestamp - start_time_stamp) - 3600; // time the process started minus now minus 1 hr in seconds we sleep for the remaining amount. 
+                if (timeleft < 0) // if below 0 just dont sleep and try again
+                    timeleft = 0;
+                System.Threading.Thread.Sleep(timeleft*1000); // every hour
+            }
+            
+        }
+    }
+}
